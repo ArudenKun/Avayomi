ac722e697f707350b4007412423b2fc88677e522
Aruden <84431560+ArudenKun@users.noreply.github.com>
2026-04-01 06:33:57 +0800

    Forgot to add AniList files

---
+++ src/Avayomi.AniList/AvayomiAniListModule.cs
@@ -0,0 +1,18 @@
+﻿using Avayomi.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Modularity;
+
+namespace Avayomi.AniList;
+
+[DependsOn(typeof(AvayomiCoreModule))]
+public sealed class AvayomiAniListModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        context
+            .Services.AddAniListClient()
+            .ConfigureHttpClient(client =>
+                client.BaseAddress = new Uri("https://graphql.anilist.co")
+            );
+    }
+}
